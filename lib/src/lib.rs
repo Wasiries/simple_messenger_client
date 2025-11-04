4cd73fd8237c03b2864292cd5d45e42be5fe96ef
Wasiries <wasiries.email@yandex.ru>
2025-11-04 10:45:21 +0300

    initial commit

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::net::TcpStream;
+use std::str;
+use std::io::{self, BufRead, BufReader, Write};
+
+static mut IP: String = String::new();
+
+pub fn initialize(ip_: String) {
+    unsafe {
+        IP = ip_;
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn begining() {
+    unsafe {
+        let mut stream = TcpStream::connect(&IP[..]).expect("Could not connect to server");
+        loop {
+            let mut input = String::new();
+            let mut buffer: Vec<u8> = Vec::new();
+            io::stdin().read_line(&mut input).expect("Failed to read from stdin");
+            stream.write(input.as_bytes()).expect("Failed to write to server");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+            let mut reader = BufReader::new(&stream);
+
+            reader.read_until(b'\n', &mut buffer).expect("Could not read into buffer");
+            print!("{}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
+        }
     }
 }
